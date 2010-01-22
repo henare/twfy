92501e6abb0fa69895f454dbd2e588730832fe76
Henare Degan <henare.degan@gmail.com>
2010-01-22 14:23:02 +1300

    Fix a pretty fundamental spelling boo-boo

--- www/docs/index.php
+++ www/docs/index.php
@@ -28,7 +28,7 @@ In the Register of Interests, Representatives and Senators declare information
 of financial interests, stocks and shares held, gifts received over a certain
 value, and memberships of Clubs and Associations.  
 </p><p>
-While we now have the register online, the current format is searchable or
+While we now have the register online, the current format isn't searchable or
 comparable. This lab provides a tool to transcribe the results from the hand
 written forms into a searchable digital format. 
 <a href="/regmem/transcriber"><b>Why not help out with the transcription, it's
