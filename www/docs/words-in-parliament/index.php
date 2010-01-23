90623cd74e6cc521ef4888bc2aa53142e98f491b
Henare Degan <henare.degan@gmail.com>
2010-01-23 23:44:11 +1300

    OA-250 Add the words-in-parliament experiment

---
+++ www/docs/words-in-parliament/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$this_page = "words-in-parliament";
+
+include_once "../../includes/easyparliament/init.php";
+
+$PAGE->page_start();
+
+?>
+<style type="text/css">
+	html, body, div#content, div#container {
+		height: 95%;
+	}
+	iframe {
+		height: 100%;
+		width: 100%;
+		padding: 0;
+		margin: 0;
+		border: 1px solid black;
+	}
+</style>
+
+<a name="frames">
+<iframe id="words-in-parliament" src="http://words-in-parliament.heroku.com/">
+</iframe>
+
+<?php $PAGE->page_end(); ?>
