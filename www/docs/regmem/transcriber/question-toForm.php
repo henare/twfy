d6b37e01ad12d782e57c812930539df67b4932cd
Tim Ansell <mithro@mithis.com>
2010-01-23 16:54:05 +1300

    Trying to make the form look nicer.

--- www/docs/regmem/transcriber/question-toForm.php
+++ www/docs/regmem/transcriber/question-toForm.php
@@ -5,15 +5,32 @@
 		<td><h2><?php echo $this->name ?></h2></td>
 		<td><?php echo $this->description ?></h2></td>
 	</tr>
-<?php foreach($this->persons as $person => $person_desc) { ?>
+<?php 
+$fields = count($this->fields);
+
+foreach($this->persons as $person => $person_desc) { ?>
 	<tr>
 		<td colspan="2">
 			<table>
+<?php 	if ($fields == 1) { ?>
 				<tr>
-					<td><h3><?php echo $person_desc; ?></h3></td>
-<?php	foreach($this->fields as $field_name => $field_desc) { ?>
+					<td colspan=<?php echo $fields; ?>><h3><?php echo $person_desc; ?></h3></td>
+<?php		foreach($this->fields as $field_name => $field_desc) { ?>
 					<td><h4><?php echo $field_desc ?></h4></td>
-<?php	} ?>
+<?php		} ?>
+<?php 	} else { 
+?>
+				<tr>
+					<td></td>
+					<td colspan=<?php echo $fields; ?>><h3><?php echo $person_desc; ?></h3></td>
+				</tr>
+				<tr>	
+					<td></td>
+<?php		foreach($this->fields as $field_name => $field_desc) { ?>
+					<td><h4><?php echo $field_desc ?></h4></td>
+<?php		}
+	}
+?>
 				</tr><tr>
 
 <?php
@@ -22,7 +39,7 @@
 		$row = @$question_responses[$i];
 ?>
 				</tr><tr>
-					<td><?php echo $i+1 ?>.</td>
+					<td class="number<?php echo ($fields > 1 ? '-single' : '-multi'); ?>"><?php echo $i+1 ?>.</td>
 <?php		foreach($this->fields as $field_name => $field_desc) { ?>
 					<td><input type="text" name="questions[<?php echo $this->name; ?>][<?php echo $person; ?>][<?php echo $i ?>][<?php echo $field_name;?>]"
 <?php 				if (end($this->fields) == $field_desc and $i == count($question_responses)) { ?>
